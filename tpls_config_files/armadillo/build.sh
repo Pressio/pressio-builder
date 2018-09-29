95b134c086057328b95aa69e441c82379520cd33
Francesco Rizzi <fr.francescorizzi@gmail.com>
2018-09-29 23:43:53 +0200

    fixing config files

--- tpls_config_files/armadillo/build.sh
+++ tpls_config_files/armadillo/build.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+SRC=/Users/fnrizzi/tpl/armadillo/armadillo-9.100.5
 PFX=/Users/fnrizzi/tpl/armadillo/install_gcc640
 CC=/opt/local/bin/gcc-mp-6
 CXX=/opt/local/bin/g++-mp-6
 
-cmake . \
+cmake -D DETECT_HDF5:BOOL=FALSE\
       -D CMAKE_CXX_COMPILER:FILEPATH=${CXX} \
       -D CMAKE_C_COMPILER:FILEPATH=${CC} \
-      -D CMAKE_INSTALL_PREFIX:PATH=${PFX}
+      -D CMAKE_INSTALL_PREFIX:PATH=${PFX} \
+      ${SRC}
 
 
