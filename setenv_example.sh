836ccf1e4a7b8f3af48fcaa86565003758c2dedc
Francesco Rizzi <fnrizzi@sandia.gov>
2019-07-02 12:42:22 +0200

    add comment to template for setenv

--- setenv_example.sh
+++ setenv_example.sh
@@ -9,4 +9,6 @@ export CC=${MPIPATH}/bin/mpicc
 export CXX=${MPIPATH}/bin/mpic++
 
 # MPIRUNe has to be set because this is what is used by scripts inside
+# This is only used if you plan to run "ctest". If you DO not plan to use ctest,
+# and run tests manually, then this is useless.
 export MPIRUNe=${MPIPATH}/bin/mpirun
