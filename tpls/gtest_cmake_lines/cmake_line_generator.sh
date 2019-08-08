c18331b865c5f1b03b20f18eadabfca761dc3228
Francesco Rizzi <fnrizzi@sandia.gov>
2019-08-08 11:07:26 +0200

    fix r_path

--- tpls/gtest_cmake_lines/cmake_line_generator.sh
+++ tpls/gtest_cmake_lines/cmake_line_generator.sh
@@ -5,7 +5,8 @@ default(){
     compilers
 
     # check if we are on mac, if so turn r_path
-    if [[ "$OSTYPE" == "darwin"* ]]; then
+    if [[ $OSTYPE == *"darwin"* ]]; then
+	echo "on mac: turning on r_path for gtest"
 	mac_r_path
     fi
     echo CMAKELINE
