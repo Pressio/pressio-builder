8fae09dcd993dfaa83f1c4943fee462d69c92c62
Francesco Rizzi <fnrizzi@sandia.gov>
2020-09-29 10:27:34 +0200

    updating for new trilinos and custom generators lines

--- tpls/all/trilinos/default_cmake_line_generator.sh
+++ tpls/all/trilinos/default_cmake_line_generator.sh
@@ -13,3 +13,16 @@ function default(){
     trilinos_kokkos_serial
     trilinos_packages_for_pressio
 }
+
+function frizzimac(){
+    trilinos_build_type
+    trilinos_link_type
+    trilinos_verbose_makefile_on
+    trilinos_mpi_c_cxx_compilers
+    trilinos_mpi_fortran_on
+    trilinos_tests_off
+    trilinos_examples_off
+    trilinos_openblaslapack
+    trilinos_kokkos_serial
+    trilinos_packages_for_pressio
+}
