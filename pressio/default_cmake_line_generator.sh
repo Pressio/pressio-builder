5b50c3ec037519950ef058619509f1226c93b5b8
Francesco Rizzi <fnrizzi@sandia.gov>
2020-11-03 10:11:20 +0100

    update cmake lines for pressio4py

--- pressio/default_cmake_line_generator.sh
+++ pressio/default_cmake_line_generator.sh
@@ -56,7 +56,6 @@ function default_pybind() {
     pressio_build_type
     pressio_cmake_verbose
     pressio_serial_c_cxx_compilers
-    pressio_enable_eigen
     pressio_enable_pybind11
     pressio_enable_debug_print
 }
@@ -65,13 +64,11 @@ function pybind11_with_tests() {
     pressio_build_type
     pressio_cmake_verbose
     pressio_serial_c_cxx_compilers
-    pressio_enable_eigen
     pressio_enable_pybind11
     pressio_enable_gtest
     pressio_tests_on
     pressio_enable_debug_print
 }
-
 
 #------------------------------------------
 # default generator function for tutorials
